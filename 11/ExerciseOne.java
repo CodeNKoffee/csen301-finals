5d30bf1e89266e6404ef396fb6757a2276a15a76
Hatem Soliman <hatemthedev@gmail.com>
2024-01-05 01:12:44 +0200

    Initial commit - 2011-ex1

---
+++ 11/ExerciseOne.java
@@ -0,0 +1,18 @@
+class ExerciseOne {
+  public static boolean binarySearch(int[] arr, int v) {
+    int min = 0;
+    int max = arr.length - 1;
+    int mid;
+
+    while (max >= min) {
+      mid = min + ((max - min) / 2);
+      if (arr[mid] == v)
+        return true;
+      else if (arr[mid] > v) 
+        max = mid - 1;
+      else 
+        min = mid + 1;
+    }
+    return false;
+  }
+}
